1f1281287c310ce71035027ecc0507b628d87c52
leaderman77 <sardorasatillaev@gmail.com>
2023-02-20 02:01:39 +0500

    #1 program is optimized, details is saved in csv, images is redownloaded

--- scrape_car_images.py
+++ scrape_car_images.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 import requests
+import pandas as pd
 from requests_html import HTMLSession
 from bs4 import BeautifulSoup
 
@@ -58,14 +59,9 @@ def parsing_img(page_url):
     return image_list
 
 
-def parsing_img_and_info(page_url):
+def parsing_img_and_info():
     """function to parse car details and images
     Details include vehicles' type, car model, year and place
-
-    Parameters
-    ----------
-    page_url : str
-        site url to read html file
 
     Returns
     -------
@@ -73,103 +69,115 @@ def parsing_img_and_info(page_url):
         return list of vehicle' type, model, year, place,
         image and plate number image
     """
-    content = read_html(page_url)
-
     results = []
-    containers = content.find_all("div", class_="panel-body")
-    for container in containers:
-        # images
-        image_tags = container.find_all('img')
-        img_urls = [img['src'] for img in image_tags if 'src' in img.attrs]
+    urls = [
+        'https://platesmania.com/uz/gallery.php?&ctype=1&start={0}',
+        'https://platesmania.com/uz/gallery.php?&ctype=1&usr=20149&start={0}',
+        'https://platesmania.com/uz/gallery.php?&ctype=1&usr=43788&start={0}',
+        'https://platesmania.com/uz/gallery.php?&ctype=1&usr=85450&start={0}',
+        'https://platesmania.com/uz/gallery.php?&ctype=1&usr=42093&start={0}',
+        'https://platesmania.com/uz/gallery.php?&ctype=1&usr=55639&start={0}',
+    ]
+    for url in urls:
+        # for each page
+        for i in range(101):
+            # set url
+            full_url = url.format(i)
+            print(full_url)
 
-        # car types
-        car_types = container.find_all("h4", class_="text-center")
-        types = [car_type.text for car_type in car_types if car_type.text is not None]
+            # soap object
+            content = read_html(full_url)
 
-        # car model
-        car_models = container.find_all("p", class_="text-center")
-        models = [model.text.replace('\n', ' ') for model in car_models if model.text is not None]
+            # extract images section from html
+            containers = content.find_all("div", class_="panel-body")
 
-        results.append([img_urls, types, models])
+            for container in containers:
+                # images
+                image_tags = container.find_all('img')
+                img_urls = [img['src'] for img in image_tags if 'src' in img.attrs]
+                number_in_text = image_tags[1]['alt']
+
+                # car types
+                car_types = container.h4.text
+
+                # car model
+                car_models = container.find_all("p", class_="text-center")
+                models = [model.text.replace('\n', ' ') for model in car_models if model.text is not None]
+
+                results.append([img_urls, number_in_text, car_types, models])
 
     return results
 
 
-def download_images(page_url):
-    """function to download and save parsed images
-
-    Parameters
-    ----------
-    page_url : str
-        site url to read html file
-
-    """
-    results = parsing_img(page_url)
-
-    cars_list = []
-    plates_list = []
-    for result in results:
-        cars_list.append(result[0])
-        plates_list.append(result[1])
-
-    print(f"Found {len(cars_list)} cars' images")
-    print(f"Found {len(plates_list)} plates' images")
-
+def download_images():
+    """function to download and save parsed images"""
     headers = {
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/78.0.3904.108 Safari/537.36 '
     }
     currentDir = os.getcwd()
+    cars_path = os.path.join(currentDir, 'cars')
+    numbers_path = os.path.join(currentDir, 'numbers')
+    cars_count = 0
+    numbers_count = 0
+    urls = [
+        'https://platesmania.com/uz/gallery.php?&ctype=1&start={0}',
+        'https://platesmania.com/uz/gallery.php?&ctype=1&usr=20149&start={0}',
+        'https://platesmania.com/uz/gallery.php?&ctype=1&usr=43788&start={0}',
+        'https://platesmania.com/uz/gallery.php?&ctype=1&usr=85450&start={0}',
+        'https://platesmania.com/uz/gallery.php?&ctype=1&usr=42093&start={0}',
+        'https://platesmania.com/uz/gallery.php?&ctype=1&usr=55639&start={0}',
+    ]
+    for url in urls:
+        # for each page
+        for i in range(101):
+            # set url
+            full_url = url.format(i)
+            print(full_url)
+            results = parsing_img(full_url)
 
-    # cars
-    count = 0
-    path = os.path.join(currentDir, 'cars')
-    if len(cars_list) != 0:
-        for image_link in enumerate(cars_list):
-            try:
-                filename = image_link[1].split('/')[-1]
-                r = requests.get(image_link[1], headers=headers, stream=True, timeout=5)
-                if r.status_code == 200:
-                    with open(os.path.join(path, filename), 'wb') as f:
-                        r.raw.decode_content = True
-                        shutil.copyfileobj(r.raw, f)
-                    count += 1
-            except Exception as e:
-                print(e)
+            for result in results:
 
-            count += 1
-        print(f" {count} the car images have been downloaded!\n")
+                # cars
+                try:
+                    filename = result[0].split('/')[-1]
+                    if not os.path.exists(os.path.join(cars_path, filename)):
+                        r = requests.get(result[0], headers=headers, stream=True, timeout=5)
+                        if r.status_code == 200:
+                            with open(os.path.join(cars_path, filename), 'wb') as f:
+                                r.raw.decode_content = True
+                                shutil.copyfileobj(r.raw, f)
+                                cars_count += 1
+                except Exception as e:
+                    print(e)
 
-    # plate numbers
-    path = os.path.join(currentDir, 'numbers')
-    count = 0
-    if len(plates_list) != 0:
-        for image_link in enumerate(plates_list):
-            try:
-                filename = image_link[1].split('/')[-1]
-                r = requests.get(image_link[1], headers=headers, stream=True, timeout=5)
-                if r.status_code == 200:
-                    with open(os.path.join(path, filename), 'wb') as f:
-                        r.raw.decode_content = True
-                        shutil.copyfileobj(r.raw, f)
-                    count += 1
-            except Exception as e:
-                print(e)
+                # plate numbers
+                try:
+                    filename = result[1].split('/')[-1]
+                    if not os.path.exists(os.path.join(numbers_path, filename)):
+                        r = requests.get(result[1], headers=headers, stream=True, timeout=5)
+                        if r.status_code == 200:
+                            with open(os.path.join(numbers_path, filename), 'wb') as f:
+                                r.raw.decode_content = True
+                                shutil.copyfileobj(r.raw, f)
+                                numbers_count += 1
+                except Exception as e:
+                    print(e)
 
-        print(f" {count} the plate number images have been downloaded!\n")
+    print(f" {cars_count} car images have been downloaded!\n")
+    print(f" {numbers_count} license plate images have been downloaded!\n")
 
 
 if __name__ == '__main__':
-    urls = [
-        'https://platesmania.com/uz/gallery.php?&ctype=1&start=',
-        'https://platesmania.com/uz/gallery.php?&ctype=1&usr=20149&start=',
-        'https://platesmania.com/uz/gallery.php?&ctype=1&usr=43788&start=',
-        'https://platesmania.com/uz/gallery.php?&ctype=1&usr=85450&start=',
-        'https://platesmania.com/uz/gallery.php?&ctype=1&usr=42093&start=',
-        'https://platesmania.com/uz/gallery.php?&ctype=1&usr=55639&start=',
-    ]
-    for url in urls:
-        for i in range(0, 100):
-            url = f'{url}{i}'
-            download_images(url)
+    print("Process has started")
+    # save images
+    # download_images()  # activate when you want to download new imgs
 
-    print("Finished")
+    # parsing vehicles' images, models, types, plate in text
+    pd.DataFrame(parsing_img_and_info()).to_csv('uz_license_plates.csv',
+                                                index_label="Index",
+                                                header=['car & license plates img',
+                                                        'license plates in txt',
+                                                        'vehicle type',
+                                                        'vehicle model & other info'])
+    print("Process has finished")
